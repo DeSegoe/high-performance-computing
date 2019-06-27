55daa44b245832df8e7b6087de34506eb1a551f7
Noloman <noloxtreme@gmail.com>
2019-06-27 14:17:46 +0200

    Aggregate operations

--- aggregate_operations.c
+++ aggregate_operations.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <omp.h>
 
-#define DATA_SIZE 1 << 26
+#define DATA_SIZE 1 << 28
 
 typedef unsigned char uchar;
 typedef unsigned int uint;
@@ -18,38 +18,102 @@ int main(int argc,char** argv) {
 
     ulong parallelCount = 0;
     ulong serialCount = 0;
-    ulong start = omp_get_wtime();
+    double serialDuration = -1;
+    double start = omp_get_wtime();
     for (uint i=0;i<DATA_SIZE;i++ ) {
         serialCount+=data[i];
     }
-    ulong end = omp_get_wtime();
-    printf("Serial operation took %u to run. The total is %u\n",end-start,serialCount);
+    double end = omp_get_wtime();
+    serialDuration = end - start;
+    printf("Serial operation took %.5f seconds to run. The total is %u, Speed up -\n",end-start,serialCount);
 
+    parallelCount = 0;
     start = omp_get_wtime();
-    ulong partialSum[32][1];
+    ulong partialArray[32];
     #pragma omp parallel
     {
         int idx = omp_get_thread_num();
         int maxThreads = omp_get_num_threads();
-        partialSum[idx][0] = 0;
+        partialArray[idx] = 0;
 
         #pragma omp block
 
         #pragma omp for
         for (uint i=0;i<DATA_SIZE;i++) {
-            partialSum[omp_get_thread_num()][0]+=data[i];
+            partialArray[omp_get_thread_num()]+=data[i];
         }
 
 
         #pragma omp master
         {
             for (int i=0;i<maxThreads;i++) {
-                parallelCount+=partialSum[i][0];
+                parallelCount+=partialArray[i];
             }
         }
     }
     end = omp_get_wtime();
-    printf("Parallel operation took %u to run. The total is %u\n",end-start,parallelCount);
+    printf("Parallel operation using partialSum[32] took %.5f seconds to run. The total is %u Speed up %.1f\n",end-start,parallelCount,serialDuration/(end-start));
+
+    parallelCount = 0;
+    start = omp_get_wtime();
+    ulong partialMatrix[32][1];
+    #pragma omp parallel
+    {
+        int idx = omp_get_thread_num();
+        int maxThreads = omp_get_num_threads();
+        partialMatrix[idx][0] = 0;
+
+        #pragma omp block
+
+        #pragma omp for
+        for (uint i=0;i<DATA_SIZE;i++) {
+            partialMatrix[omp_get_thread_num()][0]+=data[i];
+        }
+
+
+        #pragma omp master
+        {
+            for (int i=0;i<maxThreads;i++) {
+                parallelCount+=partialMatrix[i][0];
+            }
+        }
+    }
+    end = omp_get_wtime();
+    printf("Parallel operation using partialSum[32][1] took %.5f seconds to run. The total is %u Speed up %.1f\n",end-start,parallelCount,serialDuration/(end-start));
+
+    parallelCount = 0;
+    start = omp_get_wtime();
+    #pragma omp parallel
+    {
+        int maxThreads = omp_get_num_threads();
+
+        ulong threadSum = 0;
+
+        #pragma omp for
+        for (uint i=0;i<DATA_SIZE;i++) {
+            threadSum+=data[i];
+        }
+
+
+        #pragma omp master
+        {
+            for (int i=0;i<maxThreads;i++) {
+                parallelCount+=threadSum;
+            }
+        }
+    }
+    end = omp_get_wtime();
+    printf("Parallel operation using threadSum took %.5f seconds to run. The total is %u Speed up %.1f\n",end-start,parallelCount,serialDuration/(end-start));
+
+    parallelCount = 0;
+    start = omp_get_wtime();
+    parallelCount=0;
+    #pragma omp parallel for reduction(+:parallelCount)
+    for (uint i=0;i<DATA_SIZE;i++) {
+        parallelCount+=data[i];
+    }
+    end = omp_get_wtime();
+    printf("Parallel operation using reduction took %.5f seconds to run. The total is %u Speed up %.1f\n",end-start,parallelCount,serialDuration/(end-start));
 
     free(data);
 
